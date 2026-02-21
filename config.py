53b5feb00be8e26f7bf7465e9558949f4dc7feea
Rimurutempest187 <karangmerry@gmail.com>
2026-02-21 21:36:34 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+ADMIN_IDS = [int(x.strip()) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip()]
+DB_PATH = os.getenv("DB_PATH", "data/church.db")
+CREATOR_TAG = os.getenv("CREATOR_TAG", "@Enoch_777")
