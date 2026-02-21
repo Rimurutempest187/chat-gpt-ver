0c4597ed0842eb34d2ac6c8912a790e7867539eb
Rimurutempest187 <karangmerry@gmail.com>
2026-02-21 23:22:15 +0700

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-# ပြင်ဆင်ရန်: BOT_TOKEN နှင့် ADMIN_IDS ကို သင့်အချက်အလက်ဖြင့် ပြောင်းပါ။
-
-BOT_TOKEN = "8594580402:AAEmCT-YzaZJC5XHHt0nyt1WbzQhC8ApudA"
-# Admin user IDs (list of integers)
-ADMIN_IDS = [1812962224]  # Example: [11111111, 22222222]
-# Delimiter for bulk inserts (verses, quizzes)
-BULK_DELIM = "|||"
